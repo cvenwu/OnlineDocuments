433e5960ba719ee5d446185faeba64e2cd26dc5c
cvenwu <yirufeng@foxmail.com>
2024-05-12 18:28:24 +0800

    [feat]调整目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { plumeTheme } from 'vuepress-theme-plume'
 
 
 export default defineUserConfig({
-  "base": "/OnlineDocuments/"
+  base: "/OnlineDocuments/",
   // 请不要忘记设置默认语言
   lang: 'zh-CN',
   theme: plumeTheme({
